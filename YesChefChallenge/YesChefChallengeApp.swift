71fcff2958836b5975dda88a43797e8092439f47
Mac <macjacobson99@gmail.com>
2025-10-01 14:14:28 -0600

    Initial Commit

---
+++ YesChefChallenge/YesChefChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesChefChallengeApp.swift
+//  YesChefChallenge
+//
+//  Created by Jacobson, Machiah - Student on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct YesChefChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YesChefChallengeTests/YesChefChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YesChefChallengeTests.swift
+//  YesChefChallengeTests
+//
+//  Created by Jacobson, Machiah - Student on 10/1/25.
+//
+
+import Testing
+@testable import YesChefChallenge
+
+struct YesChefChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
