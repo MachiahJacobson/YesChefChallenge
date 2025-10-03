6a7451c5a9fe8efb5cd0e3662686517cc051ddbe
Mac <macjacobson99@gmail.com>
2025-10-03 13:32:24 -0600

    This is my final form of the "Yes CHef Challenge". i made 3 Tabs Views on how to make Pudding, with the home page, igredients, and instructions.

---
+++ YesChefChallenge/Ingredients .swift
@@ -0,0 +1,18 @@
+//
+//  Ingredients .swift
+//  YesChefChallenge
+//
+//  Created by Jacobson, Machiah - Student on 10/1/25.
+//
+
+import SwiftUI
+
+struct Ingredients_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ingredients_()
+}
